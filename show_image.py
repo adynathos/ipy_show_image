e38a9608a9a9e7a5cae6113137c8cbf5673f560d
Krzysztof Lis <adynathos@gmail.com>
2022-02-18 23:28:49 +0100

    image grid

--- show_image.py
+++ show_image.py
@@ -26,6 +26,7 @@ import numpy as np
 from PIL import Image as PIL_Image
 from matplotlib import cm
 from IPython.display import display_html
+from math import ceil
 
 ### Image IO
 
@@ -198,3 +199,74 @@ def show_set_default_image_format(new_fmt : str):
 show = ImageGridHTML.show_image
 show.set_default_image_format = show_set_default_image_format
 
+
+def image_montage_same_shape(imgs, num_cols=2, downsample=1, border=0, border_color=(0, 0, 0), captions=None, caption_color=(200, 128, 0), caption_size=3):
+	"""
+	example:
+	`image_montage_same_shape(
+		imgs = [
+			A, B,
+			C, D,
+		],
+		num_cols=2,
+		border = 4,
+		border_color = (128, 128, 128),
+		captions = [
+			'A', 'B',
+			'C', 'D',
+		],
+	)`
+	"""
+
+
+	num_imgs = imgs.__len__()
+	num_rows = int(np.ceil(num_imgs / num_cols))
+	
+	row_cols = np.array([num_rows, num_cols])
+
+	img_sizes = np.array([
+		img.shape[:2] for img in imgs
+	], dtype=np.int32) // downsample
+	
+	img_size_biggest = np.max(img_sizes, axis=0) 
+	img_size_with_border = img_size_biggest + border
+
+	full_size = (num_rows * img_size_biggest[0] + (num_rows-1)*border, num_cols * img_size_biggest[1] + (num_cols-1)*border, 3)
+
+	out = np.full(full_size, fill_value=border_color, dtype=np.uint8)
+
+	row_col_pos = np.array([0, 0])
+
+	for idx, img in enumerate(imgs):
+		# none means black section
+		if img is not None:
+			#img = ensure_numpy_image(img)
+			if downsample != 1:
+				img = img[::downsample, ::downsample]
+			
+			img = adapt_img_data(img)
+			if img.shape.__len__() == 2:
+				img = np.tile(img[:, :, None], (1, 1, 3))
+
+			if captions is not None and captions[idx]:
+				import cv2 as cv
+				# print('img', img.shape, img.dtype)
+				caption = captions[idx]
+				img = img.copy()
+				# shadow
+				cv.putText(img, caption, (18, 50), cv.FONT_HERSHEY_DUPLEX, caption_size / downsample, color=(0, 0, 0), thickness=ceil(4/downsample))
+				# foreground
+				cv.putText(img, caption, (18, 50), cv.FONT_HERSHEY_DUPLEX, caption_size / downsample, color=caption_color, thickness=ceil(2/downsample))
+
+			img_sz = img_sizes[idx]
+			tl = img_size_with_border * row_col_pos
+			br = tl + img_sz
+
+			out[tl[0]:br[0], tl[1]:br[1]] = img[:img_sz[0], :img_sz[1]]
+		
+		row_col_pos[1] += 1
+		if row_col_pos[1] >= num_cols:
+			row_col_pos[0] += 1
+			row_col_pos[1] = 0
+
+	return out
